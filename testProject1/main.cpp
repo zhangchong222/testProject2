579e22f18cfb2d439fef68ff216254d7055dd63d
zhangchong <2681782985@qq.com>
2025-05-02 23:35:18 +0800

    添加项目文件。

---
+++ testProject1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+	int a = 0;
+	int b = 2;
+	int c = a + b;
+	cout << "×ÜºÍ£º" << c << std::endl;
+	Sleep(2000);
+	
+}
